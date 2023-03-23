9054ee099574736f9e36f9a3740dfe7f95929e13
Julien Carayon <juliencarayon@outlook.com>
2023-03-23 14:39:51 +0100

    try #1 to use chronobio as submodule

--- _launch.sh
+++ _launch.sh
@@ -1,8 +1,11 @@
 FILE=aigrisculteurs/chronobio 
 
-if [ -f "$FILE" ]; then
-    rm -rf aigrisculteurs/chronobio 
+if [ -d "$FILE" ]; then
+    rm -rf $FILE
 fi
-ln -s ../../../chronobio aigrisculteurs/chronobio #Create a symlink to prevent useless folder redundency.
+ln -s dependencies/chronobio/chronobio aigrisculteurs/chronobio #Create a symlink to prevent useless folder redundency.
+
+git submodule init
+git submodule update --init --recursive
 
 python3 aigrisculteurs/aigrisculteurs_client.py -a 127.0.0.1 -p $1 -u aigrisculteurs
