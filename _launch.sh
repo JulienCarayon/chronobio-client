0d1f87aa290010e75e3e67fc11abae3129bdae78
Julien Carayon <juliencarayon@outlook.com>
2023-03-23 17:07:10 +0100

    :sparkles: making _launch.sh work with submodule

--- _launch.sh
+++ _launch.sh
@@ -1,11 +1,14 @@
-FILE=aigrisculteurs/chronobio 
-
-if [ -d "$FILE" ]; then
-    rm -rf $FILE
-fi
-ln -s dependencies/chronobio/chronobio aigrisculteurs/chronobio #Create a symlink to prevent useless folder redundency.
-
 git submodule init
 git submodule update --init --recursive
+
+FILE=aigrisculteurs/chronobio 
+echo $FILE
+if [ -d "$FILE" ]; then
+    echo "chronobio symlink exists, deleting"
+    rm -rf $FILE
+fi
+ln -s -r dependencies/chronobio/chronobio aigrisculteurs/chronobio #Create a symlink to prevent useless folder redundency.
+
+
 
 python3 aigrisculteurs/aigrisculteurs_client.py -a 127.0.0.1 -p $1 -u aigrisculteurs
