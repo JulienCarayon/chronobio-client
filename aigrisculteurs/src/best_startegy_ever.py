c1737071cda359c1a7f69fb46e51903de1e93efd
Dorian <54947603+DorianFournier@users.noreply.github.com>
2023-03-30 12:09:28 +0200

    Fix tests file tree (#17)
    * Moved folders and added tests
    
    * Updated dependenciens
    
    * fix pytest module name
    
    ---------
    
    Co-authored-by: Julien Carayon <78759372+JulienCarayon@users.noreply.github.com>

--- aigrisculteurs/src/best_startegy_ever.py
+++ aigrisculteurs/src/best_startegy_ever.py
@@ -1,6 +1,7 @@
 import logging
 
-from constants import MAXIMUM_FIELDS_NUMBER, LOCATION, FIELDS, NEEDED_WATER, WORKERS
+
+from src.constants import MAXIMUM_FIELDS_NUMBER, LOCATION, FIELDS, NEEDED_WATER, WORKERS
 
 logging.basicConfig(
     filename="aigrisculteurs.log",
