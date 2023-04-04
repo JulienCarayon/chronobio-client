72decebbd1c53f9b5addc0a3f3673d43a1b37cc1
DorianFournier <dorian.fournier@ynov.com>
2023-04-05 01:07:05 +0200

    :white_check_mark:pre-commit implementation done

--- aigrisculteurs/src/best_strategy_ever.py
+++ aigrisculteurs/src/best_strategy_ever.py
@@ -25,7 +25,6 @@ from src.constants import (
 
 logging.basicConfig(
     filename="../aigrisculteurs.log",
-    encoding="utf-8",
     level=logging.DEBUG,
     format="%(asctime)s [%(levelname)-8s] %(filename)20s(%(lineno)3s):%(funcName)-20s :: %(message)s",  # noqa: E501
     datefmt="%m/%d/%Y %H:%M:%S",
